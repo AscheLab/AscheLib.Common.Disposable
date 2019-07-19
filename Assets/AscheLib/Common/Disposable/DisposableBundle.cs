9304bcd1f314a268540eeb0cd1b4a2aee1d5f9ba
AscheLab <tfb00plzdj+aschelab@gmail.com>
2019-07-19 16:03:48 +0900

    Create Project

---
+++ Assets/AscheLib/Common/Disposable/DisposableBundle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AscheLib {
+	public class DisposableBundle : IDisposable {
+		readonly List<IDisposable> _list = new List<IDisposable>();
+		public void Add(params IDisposable[] disposables) {
+			foreach(var d in disposables) {
+				_list.Add(d);
+			}
+		}
+		#region IDisposable Support
+		bool _disposedValue = false;
+		protected virtual void Dispose(bool disposing) {
+			if(!_disposedValue) {
+				if(disposing) {
+					_list.ForEach(d => d.Dispose());
+				}
+				_disposedValue = true;
+			}
+		}
+		~DisposableBundle() {
+			Dispose(false);
+		}
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
---
+++ Assets/AscheLib/Common/Disposable/GameObjectDisposable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace AscheLib {
+	public class GameObjectDisposable : IDisposable {
+		public readonly GameObject _target;
+
+		public GameObjectDisposable(GameObject go) {
+			_target = go;
+		}
+		#region IDisposable Support
+		bool _disposedValue = false;
+		protected virtual void Dispose(bool disposing) {
+			if(!_disposedValue) {
+				if(disposing) {
+					GameObject.Destroy(_target);
+				}
+				_disposedValue = true;
+			}
+		}
+		~GameObjectDisposable() {
+			Dispose(false);
+		}
+		public void Dispose() {
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
